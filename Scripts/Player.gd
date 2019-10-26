38f7b3d97577ba480c333963499878e77703114c
FlavioFS <flaviofreitas.h@gmail.com>
2019-10-26 14:50:26 -0300

    Player controller tweeks, Sound loop reimport fix

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,17 +1,18 @@
 extends KinematicBody2D
 
-const UP = Vector2(0,-1)
-const GRAVITY = 300
-const FRICTION = 0
+signal animate
+
 const MAX_SPEED = 1000
-const MAX_FALL = 0
-const ACCELERATION = 1000
-const DECELERATION = 0
-const JUMP_FORCE = -4000
 const BOOST_MULTIPLIER = 1.5
 const WORLD_LIMIT = 4000
 const JUMP_SFX = preload("res://SFX/jump1.ogg")
 const HURT_SFX = preload("res://SFX/hurt.ogg")
+const FLOAT_PRECISION: float = 0.05
+
+export(float, -3000, -1000, 10) var jump_force: float = -2500
+export(float, 0, 200, 5) var gravity: float = 125
+export(float, 0, 1000, 10) var base_move_speed: float = 300
+export(float, 0.0, 1.0, 0.01) var damping: float = 0.7
 
 var velocity = Vector2(0,0)
 var lives = 5
@@ -22,15 +23,12 @@ var direction = 1
 var input_direction = 0
 var speed = Vector2(0,0)
 
-signal animate
-
 func _physics_process(delta):
-	move_and_slide(velocity, UP)
+	move_and_slide(velocity, Vector2.UP, true)
 	apply_gravity(delta)
-	move()
+	move_horizontally()
 	jump()
 	animate()
-
 
 func apply_gravity(delta):
 	if position.y > WORLD_LIMIT:
@@ -40,63 +38,43 @@ func apply_gravity(delta):
 	elif is_on_ceiling() and velocity.y < 0:
 		velocity.y = 1
 	else:
-		velocity.y += GRAVITY
+		velocity.y += gravity
 
-
-func move():
-	if Input.is_action_pressed("Left") and not Input.is_action_pressed("Right"):
-		velocity.x = max(velocity.x - ACCELERATION, -MAX_SPEED)
-	elif Input.is_action_pressed("Right") and not Input.is_action_pressed("Left"):
-		velocity.x = min(velocity.x + ACCELERATION, MAX_SPEED)
-	else:
-		velocity.x = 0
-
+func move_horizontally():
+	velocity.x  = 0 if abs(velocity.x) < FLOAT_PRECISION else velocity.x * damping
+	if Input.is_action_pressed("Left"):
+		velocity.x -= base_move_speed
+	if Input.is_action_pressed("Right"):
+		velocity.x += base_move_speed
+	velocity.x = min(max(velocity.x, -MAX_SPEED), MAX_SPEED)
 
 func jump(): # Fix double jump bug
-	if Input.is_action_just_pressed("Jump") and jump_count < extra_jumps:
-		velocity.y = JUMP_FORCE
-		play_SFX(JUMP_SFX)
-		jump_count += 1
+	if Input.is_action_just_pressed("Jump"):
+		if jump_count < extra_jumps:
+			velocity.y = jump_force
+			jump_count += 1
+			play_SFX(JUMP_SFX)
 	if is_on_floor():
 		jump_count = 0
-		
-#	if is_on_floor() and Input.is_action_pressed("Jump"):
-#		velocity.y = JUMP_FORCE
-#		play_SFX(JUMP_SFX)
-
 
 func animate():
 	emit_signal("animate", velocity)
 
-
 func hurt():
 	add_extra_frame()
-	velocity.y = JUMP_FORCE
+	velocity.y = jump_force
 	play_SFX(HURT_SFX)
 	lives -= 1
 
-
 func boost():
 	add_extra_frame()
-	velocity.y = JUMP_FORCE * BOOST_MULTIPLIER
+	velocity.y = jump_force * BOOST_MULTIPLIER
 	play_SFX(JUMP_SFX)
 
-
 func play_SFX(FILE):
-	if not $SFX.playing:
-		$SFX.stream = FILE
-		$SFX.play()
-
+	$SFX.stream = FILE
+	$SFX.play()
 
 func add_extra_frame():
 	position.y -= 1
 	yield(get_tree(), "idle_frame")
-
-
-
-
-
-
-
-
-
