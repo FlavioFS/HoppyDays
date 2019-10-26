72b3469623e86095c6b1d35429de7ffee941a820
FlavioFS <flaviofreitas.h@gmail.com>
2019-10-26 15:24:57 -0300

    Double jump hotfix :grin:

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,6 +10,7 @@ const HURT_SFX = preload("res://SFX/hurt.ogg")
 const FLOAT_PRECISION: float = 0.05
 
 export(float, -3000, -1000, 10) var jump_force: float = -2500
+export(int, 1, 10, 1) var total_jumps: int = 1
 export(float, 0, 200, 5) var gravity: float = 125
 export(float, 0, 1000, 10) var base_move_speed: float = 300
 export(float, 0.0, 1.0, 0.01) var damping: float = 0.7
@@ -18,10 +19,11 @@ var velocity = Vector2(0,0)
 var lives = 5
 
 var jump_count = 0
-var extra_jumps = 1
 var direction = 1
 var input_direction = 0
 var speed = Vector2(0,0)
+
+onready var floor_cooldown_timer: Timer = $FloorCooldownTimer
 
 func _physics_process(delta):
 	move_and_slide(velocity, Vector2.UP, true)
@@ -48,13 +50,22 @@ func move_horizontally():
 		velocity.x += base_move_speed
 	velocity.x = min(max(velocity.x, -MAX_SPEED), MAX_SPEED)
 
-func jump(): # Fix double jump bug
+func refresh_floor_cooldown():
+	if not is_on_floor():
+		floor_cooldown_timer.start()
+
+func jump():
 	if Input.is_action_just_pressed("Jump"):
-		if jump_count < extra_jumps:
+		if jump_count < total_jumps:
 			velocity.y = jump_force
 			jump_count += 1
 			play_SFX(JUMP_SFX)
-	if is_on_floor():
+
+	# Double jump hotfix: "and velocity.y >= 0"
+	# is_on_floor() randomly triggers the next frame
+	# right after jump_force action is applied
+	# (specially when jump key is pressed and released really fast)
+	if is_on_floor() and velocity.y >= 0:
 		jump_count = 0
 
 func animate():
